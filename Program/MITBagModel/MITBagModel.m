f706e2af2022fc2e3d6f7be98585b5326cab4146
SerialCore <zwx.atomx@outlook.com>
2020-12-19 23:51:47 +0800

    consider CMI mixing

--- Program/MITBagModel/MITBagModel.m
+++ Program/MITBagModel/MITBagModel.m
@@ -24,6 +24,8 @@ ClearAll @@ Names["MITBagModel`*"];
 
 MassVector::usage="MassVector[] returns mass vactor"
 ChargeVector::usage="ChargeVector[] returns charge vactor"
+BagConstant::usage="BagConstant[] return bag constant"
+ZPEConstant::usage="ZPEConstant[] return zpe constant"
 
 Hadron::usage="Hadron[structure, colorspin, correction] returns mass, radius, x"
 HadronFitting::usage="HadronFitting[structure, colorspin, m, B, Z, correction] returns mass, radius, x"
@@ -39,31 +41,35 @@ MassVector[]:=Return[{5.093,1.641,0.279,0,0}]
 
 ChargeVector[]:=Return[{-1/3,2/3,-1/3,2/3,-1/3}]
 
-Hadron[structure_,colorspin_,correct_]:=HadronFitting[structure,colorspin,MassVector[],0.145^4,1.83,correct]
+BagConstant[]:=Return[0.145^4]
+
+ZPEConstant[]:=Return[1.83]
+
+Hadron[structure_,colorspin_,correct_]:=HadronFitting[structure,colorspin,MassVector[],BagConstant[],ZPEConstant[],correct]
 
 HadronFitting[structure_,colorspin_,mass_,bagconstant_,zpe_,correction_]:=
 	Module[{n=structure,CMI=colorspin,m=mass,B=bagconstant,Z=zpe,cor=correction},
 		Clear[x,R];
 		
-		(* n: number of b, c, s, q quarks or antiquarks *)
-		(* m: mass of b, c, s, q quarks or antiquarks *)
+		(* n: number of b, c, s, n quarks or antiquarks *)
+		(* m: mass of b, c, s, n quarks or antiquarks *)
 		(* B: bag constant *)
 		(* Z: zpe *)
 		
 		(* momentum of b, c, s, q quarks *)
 		x={3.08,2.94,2.48,2.04};
 		
+		(* functions *)
+		\[Omega][mi_,xi_,r_]:=(mi^2+xi^2/r^2)^(1/2);
+		\[Mu][mi_,xi_,r_]:=r/6 (4\[Omega][mi,xi,r]r+2mi*r-3)/(2\[Omega][mi,xi,r]r(\[Omega][mi,xi,r]r-1)+mi*r);
+		y[xi_]:=xi-Sin[xi]Cos[xi];
+		A[xi_,xj_]:=1+(xi*Sin[xi]^2-3/2 y[xi])^-1 (xj*Sin[xj]^2-3/2 y[xj])^-1 (-(3/2)y[xi]*y[xj]-2xi*xj*Sin[xi]^2 Sin[xj]^2+1/2 xi*xj(2xi*SinIntegral[2xi]+2xj*SinIntegral[2xj]-(xi+xj)SinIntegral[2(xi+xj)]-(xi-xj)SinIntegral[2(xi-xj)]));
+		M[r_]:=Sum[n[[q]]*\[Omega][m[[q]],x[[q]],r],{q,1,4}]+(4\[Pi]*r^3)/3 B-Z/r-3*CouplingConstant[r]*Sum[CMI[[p,q]]*(\[Mu][m[[p]],x[[p]],r]*\[Mu][m[[q]],x[[q]],r])/r^3 A[x[[p]],x[[q]]],{p,1,4},{q,1,4}]+cor;
+		
 		(* use iteration to calculate the minimal mass with precise momentum x *)
-		For[i=0,i<3,i++,
-			(* define M(R) with new x every time *)
-			\[Omega][mi_,xi_,R_]=(mi^2+xi^2/R^2)^(1/2);
-			\[Mu][mi_,xi_,R_]=R/6 (4\[Omega][mi,xi,R]*R+2mi*R-3)/(2\[Omega][mi,xi,R]*R(\[Omega][mi,xi,R]*R-1)+mi*R);
-			y[xi_]=xi-Sin[xi]Cos[xi];
-			A[xi_,xj_,R_]=1+(xi*Sin[xi]^2-3/2 y[xi])^-1 (xj*Sin[xj]^2-3/2 y[xj])^-1 (-(3/2)y[xi]*y[xj]-2xi*xj*Sin[xi]^2 Sin[xj]^2+1/2 xi*xj(2xi*SinIntegral[2xi]+2xj*SinIntegral[2xj]-(xi+xj)SinIntegral[2(xi+xj)]-(xi-xj)SinIntegral[2(xi-xj)]));
-			M[r_]=Sum[n[[q]]*\[Omega][m[[q]],x[[q]],r],{q,1,4}]+(4\[Pi]*r^3)/3 B-Z/r-3*CouplingConstant[r]*Sum[CMI[[p,q]]*(\[Mu][m[[p]],x[[p]],r]*\[Mu][m[[q]],x[[q]],r])/r^3 A[x[[p]],x[[q]],r],{p,1,4},{q,1,4}]+cor;
-			
+		For[loop=0,loop<3,loop++,
 			(* perform variation *)
-			minimal = Minimize[{M[R],0<R<6},R];
+			minimal = Minimize[{M[R],0<R<7},R];
 			x[[1]]=X /.FindRoot[Tan[X]==X/(1-m[[1]]*(R/.Last[minimal])-(m[[1]]^2 (R/.Last[minimal])^2+X^2)^(1/2)),{X,2.04,3.14}];
 			x[[2]]=X /.FindRoot[Tan[X]==X/(1-m[[2]]*(R/.Last[minimal])-(m[[2]]^2 (R/.Last[minimal])^2+X^2)^(1/2)),{X,2.04,3.14}];
 			x[[3]]=X /.FindRoot[Tan[X]==X/(1-m[[3]]*(R/.Last[minimal])-(m[[3]]^2 (R/.Last[minimal])^2+X^2)^(1/2)),{X,2.04,3.14}];
@@ -71,12 +77,12 @@ HadronFitting[structure_,colorspin_,mass_,bagconstant_,zpe_,correction_]:=
 		];
 
 		(* the final result of mass, x, radius, and coupling constant *)
-		{First[minimal],R/. Last[minimal],x}
+		{First[minimal],R/.Last[minimal],x}
 	]
 	
 CouplingConstant[radius_]:=
-	Module[{R=radius},
-		Return[0.296/Log[1+1/(R*0.281)]]
+	Module[{r=radius},
+		Return[0.296/Log[1+1/(r*0.281)]]
 	]
 	
 ChargeRadius[qstructure_,antistructure_,hadron_]:=
@@ -86,8 +92,8 @@ ChargeRadius[qstructure_,antistructure_,hadron_]:=
 		R=hd[[2]];
 		x={hd[[3,1]],hd[[3,2]],hd[[3,3]],hd[[3,4]],hd[[3,4]]};
 		
-		\[Omega][mi_,xi_,R_]=(mi^2+xi^2/R^2)^(1/2);
-		r2[mi_,xi_,R_]=R^2 ((\[Omega][mi,xi,R]R(2xi^2 (\[Omega][mi,xi,R]R-1)+4\[Omega][mi,xi,R]R+2mi*R-3)-3/2 mi*R(4\[Omega][mi,xi,R]R+2mi*R-2xi^2-3))/(3xi^2 (2\[Omega][mi,xi,R]R(\[Omega][mi,xi,R]R-1)+mi*R)));
+		\[Omega][mi_,xi_,r_]=(mi^2+xi^2/r^2)^(1/2);
+		r2[mi_,xi_,r_]=r^2 ((\[Omega][mi,xi,r]r(2xi^2 (\[Omega][mi,xi,r]r-1)+4\[Omega][mi,xi,r]r+2mi*r-3)-3/2 mi*r(4\[Omega][mi,xi,r]r+2mi*r-2xi^2-3))/(3xi^2 (2\[Omega][mi,xi,r]r(\[Omega][mi,xi,r]r-1)+mi*r)));
 		Abs[Sum[qn[[q]]*Q[[q]]*r2[m[[q]],x[[q]],R]-an[[q]]*Q[[q]]*r2[m[[q]],x[[q]],R],{q,1,5}]]^(1/2)/5.067
 	]
 	
@@ -98,8 +104,8 @@ MagneticMoment[qstructure_,antistructure_,hadron_]:=
 		R=hd[[2]];
 		x={hd[[3,1]],hd[[3,2]],hd[[3,3]],hd[[3,4]],hd[[3,4]]};
 		
-		\[Omega][mi_,xi_,R_]=(mi^2+xi^2/R^2)^(1/2);
-		\[Mu][mi_,xi_,R_]=R/6 (4\[Omega][mi,xi,R]R+2mi*R-3)/(2\[Omega][mi,xi,R]R(\[Omega][mi,xi,R]R-1)+mi*R);
+		\[Omega][mi_,xi_,r_]=(mi^2+xi^2/r^2)^(1/2);
+		\[Mu][mi_,xi_,r_]=r/6 (4\[Omega][mi,xi,r]r+2mi*r-3)/(2\[Omega][mi,xi,r]r(\[Omega][mi,xi,r]r-1)+mi*r);
 		Sum[qn[[q]]*Q[[q]]*\[Mu][m[[q]],x[[q]],R]-an[[q]]*Q[[q]]*\[Mu][m[[q]],x[[q]],R],{q,1,5}]
 	]
 
